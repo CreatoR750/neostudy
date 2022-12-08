320f000f74d43f8ff3193248f316dd7f3fbe79e7
CreatoR750 <88798010+CreatoR750@users.noreply.github.com>
2022-12-08 22:21:17 +0300

    Fixed const

--- Module_1/scripts/script.js
+++ Module_1/scripts/script.js
@@ -1,5 +1,5 @@
-let burger = document.querySelector(".header__burger");
-let menu = document.querySelector(".header__menu");
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
