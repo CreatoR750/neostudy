1c298e3cf9fada79730d7bc764cea1d828cd7af1
CreatoR750 <88798010+CreatoR750@users.noreply.github.com>
2022-12-20 05:32:03 +0300

    Task 2

---
+++ Module_2/scripts/slider.js
@@ -0,0 +1,49 @@
+const slider = document.querySelector(".slider__wrapper");
+const prev = document.querySelector(".slider__buttons__prev");
+const next = document.querySelector(".slider__buttons__next");
+const NEWS_URL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=99f1e04ddffc4b4da15ec80bf056a183";
+let offset = 0;
+let newsLength;
+const cardWidth = 400;
+let length;
+
+const getData = async () => {
+    const response = await fetch(NEWS_URL);
+    const data = await response.json();
+    newsLength = data.articles.length;
+    length = newsLength - 2;
+    data.articles.map((article) => {
+        const div = document.createElement("div");
+        div.classList.add("slider__card");
+        div.onclick = () => window.open(article.url, "_blank");
+        div.innerHTML = `<img class="slider__card__img" src=${article.urlToImage} alt="News image" onerror="this.onerror=null;this.src='https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_01/2705191/nbc-social-default.png';" />
+        <div class="slider__card__title">
+            ${article.title}
+        </div>
+        <div class="slider__card__description">
+        ${article.title}
+        </div>`;
+        slider.appendChild(div);
+    });
+};
+
+next.addEventListener("click", () => {
+    prev.classList.remove("disabled");
+    offset = offset + cardWidth;
+    if (offset >= cardWidth * length) {
+        next.classList.add("disabled");
+        offset = cardWidth * length;
+    }
+    slider.style.left = -offset + "px";
+});
+
+prev.addEventListener("click", () => {
+    next.classList.remove("disabled");
+    offset = offset - cardWidth;
+    if (offset <= 0) {
+        prev.classList.add("disabled");
+    }
+    slider.style.left = -offset + "px";
+});
+
+getData();
